264e82cf6bd51fb8e22912a0a4c994d7f86de90f
Andrew <ahtz@mit.edu>
2015-06-17 23:36:37 -0700

    Started new repo, added assets and project settings

---
+++ Assets/Classes/MainController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour {
+
+	//50 fps
+	private float framePeriod = 0.02f;
+
+	//Board Data
+	private float boardHeight = 8.0f;
+	private float boardWidth = 5.0f;
+
+	//All data for units
+	private int unitN = 3;
+	private UnitData[] unitData = new UnitData[4]; 
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+	//initialize data for units
+	private void initUnitData()
+	{
+		// base
+		unitData [1] = new UnitData (1, 150, 0.0f, 0.0f, 0, 0, 0.0f, 0.0f, 0.0f);
+
+		// marine
+		unitData [2] = new UnitData (2, 50, 0.1f, 0.6f, 10, 0, 0.0f, 0.0f, 0.0f);
+
+		//zergling
+		unitData [3] = new UnitData (3, 50, 0.2f, 0.2f, 10, 0, 0.0f, 0.0f, 0.0f);
+	}
+}
+
---
+++ Assets/Classes/UnitData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitData {
+	private int type = 0;
+	private int health = 0;
+	private float moveSpeed = 0.0f;
+	private float attackRange = 0.0f;
+	private int attackDamage = 0;
+	private int team = 0;
+	private float x = 0.0f;
+	private float y = 0.0f;
+	private float rotato = 0.0f;
+
+	public UnitData(int tType, int tHealth, float tMoveSpeed, float tAttackRange, int tAttackDamage, int tTeam, float tX, float tY, float tRotato)
+	{
+		type = tType;
+		health = tHealth;
+		moveSpeed = tMoveSpeed;
+		attackRange = tAttackRange;
+		attackDamage = tAttackDamage;
+		team = tTeam;
+		x = tX;
+		y = tY;
+		rotato = tRotato;
+	}
+
+}
